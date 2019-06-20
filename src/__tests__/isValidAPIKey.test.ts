ba4d3395acda9fc82053127ec53b402128e46fc1
Janek Rahrt <janek.rahrt@me.com>
2019-06-20 18:50:39 +0200

    v2.0.2

--- src/__tests__/isValidAPIKey.test.ts
+++ src/__tests__/isValidAPIKey.test.ts
@@ -8,4 +8,5 @@ test('isValidAPIKey', () => {
   expect(isValidAPIKey('ev-df799342fa9405da27916bcc96e46a5e')).toBe(true)
   expect(isValidAPIKey('ev-cd180873104fe2eb1edb20297b8d024e')).toBe(true)
   expect(isValidAPIKey('ev-416b1aa14fd1d8bed9b53979bbb64293')).toBe(true)
+  expect(isValidAPIKey('df-416b1aa14fd1d8bed9b53979bbb64293')).toBe(true)
 })
--- src/isValidAPIKey.ts
+++ src/isValidAPIKey.ts
@@ -1,3 +1,2 @@
 export const isValidAPIKey = (apikey: string): boolean =>
-  apikey.substring(0, 3) === 'ev-' &&
-  /^[a-f0-9]{32}$/g.test(apikey.substring(3))
+  apikey.substring(2, 3) === '-' && /^[a-f0-9]{32}$/g.test(apikey.substring(3))
